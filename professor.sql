4003c60c6f075fb36aafe9a10a8125dedd4059e4
PedroAndresCondeValdez <pedroandrescondevaldez@gmail.com>
2021-05-27 18:39:43 -0400

    Termino Professor.sql

--- professor.sql
+++ professor.sql
@@ -1,5 +1,7 @@
 /** Creamos la tabla professor que sera utilizada en la aplicacion
-    MySql Workbench, lo cual nos ayudara a hacer todo el codigo*/
+    MySql Workbench, lo cual nos ayudara a hacer todo el codigo
+    Implementando los datos que pidio.
+    */
 CREATE TABLE professor
 (id INT NOT NULL AUTO_INCREMENT,
  first_name VARCHAR(50),
